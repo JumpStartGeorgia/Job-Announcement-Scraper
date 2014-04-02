eff3b552a05a0f6133a166bc674e666e9524f043
jason <jason.addie@gmail.com>
2014-04-02 12:05:28 +0400

    cleaned up code a bit

--- hr.gov.ge.rb
+++ hr.gov.ge.rb
@@ -19,7 +19,7 @@ def create_directory(file_path)
 	end
 end
 
-start = Time.now
+@start = Time.now
 
 # log file to record messages
 # delete existing log file
@@ -83,25 +83,6 @@ def get_end_id
   @log.info "end id = #{id}"
   id
 end
-
-# start / end ids
-start_id = get_start_id
-end_id = get_end_id
-
-# if start id is >= end id, stop
-if start_id >= end_id
-  @log.info "------------------------------"
-  @log.info "The start id is >= end id, so there is nothing to do."
-  @log.info "------------------------------"
-  exit
-end
-
-# total number of records to process 
-# is end - start * 2,
-# where 2 represents the 
-total_to_process = (end_id - start_id + 1) * @locales.length
-total_left_to_process = (end_id - start_id + 1) * @locales.length
-@log.info "There are #{total_to_process} records to process"
 
 ####################################################
 
@@ -314,47 +295,103 @@ end
 
 ##########################
 
-#initiate hydra
-hydra = Typhoeus::Hydra.new(max_concurrency: 20)
-request = nil
+def make_requests(ids)
 
-#build hydra queue
-(start_id..end_id).map do |i|
-  @locales.each do |locale|
-    url = @urls[locale][:url] + i.to_s
-    request = Typhoeus::Request.new("#{url}", followlocation: true)
+  # total number of records to process 
+  # is the ids length * locales.length
+  total_to_process = ids.length * @locales.length
+  total_left_to_process = ids.length * @locales.length
+  @log.info "There are #{total_to_process} records to process"
 
-    request.on_complete do |response|
-      if response.success?
-        # put success callback here
-        @log.info("#{response.request.url} - success")
+
+  #initiate hydra
+  hydra = Typhoeus::Hydra.new(max_concurrency: 20)
+  request = nil
+
+  #build hydra queue
+  ids.each do |i|
+    @locales.each do |locale|
+      url = @urls[locale][:url] + i.to_s
+      request = Typhoeus::Request.new("#{url}", followlocation: true)
+
+      request.on_complete do |response|
+        if response.success?
+          # put success callback here
+          @log.info("#{response.request.url} - success")
+          
+          # process the response        
+          process_response(response)
+        elsif response.timed_out?
+          # aw hell no
+          @log.warn("#{response.request.url} - got a time out")
+        elsif response.code == 0
+          # Could not get an http response, something's wrong.
+          @log.error("#{response.request.url} - no response: #{response.return_message}")
+        else
+          # Received a non-successful http response.
+          @log.error("#{response.request.url} - HTTP request failed: #{response.code.to_s}")
+        end
         
-        # process the response        
-        process_response(response)
-      elsif response.timed_out?
-        # aw hell no
-        @log.warn("#{response.request.url} - got a time out")
-      elsif response.code == 0
-        # Could not get an http response, something's wrong.
-        @log.error("#{response.request.url} - no response: #{response.return_message}")
-      else
-        # Received a non-successful http response.
-        @log.error("#{response.request.url} - HTTP request failed: #{response.code.to_s}")
+        # decrease counter of items to process
+        total_left_to_process -= 1
+        if total_left_to_process == 0
+          @log.info "------------------------------"
+          @log.info "It took #{Time.now - @start} seconds to process #{total_to_process} items"
+          @log.info "------------------------------"
+        end
       end
-      
-      # decrease counter of items to process
-      total_left_to_process -= 1
-      if total_left_to_process == 0
-        @log.info "------------------------------"
-        @log.info "It took #{Time.now - start} seconds to process #{total_to_process} items"
-        @log.info "------------------------------"
-      end
+
+
+      hydra.queue(request)
     end
+  end
+
+  hydra.run
+
+end
+
+####################################
+# process any new items
+####################################
+# get the start / end ids
+start_id = get_start_id
+end_id = get_end_id
+# if start id is >= end id, stop
+if start_id >= end_id
+  @log.info "------------------------------"
+  @log.info "The start id is >= end id, so there is nothing to do."
+  @log.info "------------------------------"
+  exit
+else
+  # get the information
+  make_requests((start_id..end_id).to_a)
+end
 
 
-    hydra.queue(request)
+=begin
+####################################
+# look for items on file, but with no json file
+# if found, try reprocessing them
+####################################
+
+# get all item folders
+puts 'getting all folders'
+folders = Dir.glob(@data_path + "*")
+all_ids = nil
+if Dir.exists?(@data_path) && !folders.empty?
+puts 'pulling out folder ids'
+  all_ids = folders.map{|x| x.split('/')[2].to_i}.sort
+end
+
+# get all items that have json file
+puts 'getting all folders with json'
+folders = Dir.glob(File.join(@data_path,"**","*.json"))
+good_ids = nil
+if Dir.exists?(@data_path) && !folders.empty?
+  puts "found #{folders.length} folders"
+  (0..5).each do |i|
+    puts folders[i]
   end
 end
 
-hydra.run
-
+=end
